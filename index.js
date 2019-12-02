ef394ebfb1936fd0fc1ff3c6600f468f3bc24b3b
Jacob Roessler <jroess852@gmail.com>
2019-12-02 02:48:38 -0500

    Added fun fact javascript to index html file

---
+++ index.js
@@ -0,0 +1,27 @@
+
+var HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() { 
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        }
+        anHttpRequest.onerror = (err) =>{
+            var p = document.createElement("p");
+            p.innerHTML = "Adblock usually causes facts to stop working";
+            document.getElementById('facts').append(p);
+        }
+
+        anHttpRequest.open( "GET", aUrl, true );            
+        anHttpRequest.send( null );
+    }
+}
+var client = new HttpClient();
+client.get(`http://numbersapi.com/${parseInt(Math.random()*500)}/`
+, function(response) {
+    console.log(response);
+    var p = document.createElement("p");
+    p.innerHTML = response;
+    document.getElementById('facts').append(p);
+});
+
